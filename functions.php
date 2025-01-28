9c95866f25b62bed9115cfb52e263606cb479ecd
Aidan Y <jamesayipt6@gmail.com>
2025-01-28 12:23:59 -0800

    Day 5 lecture changes

--- functions.php
+++ functions.php
@@ -23,18 +23,32 @@ function mindset_enqueues() {
         array(),
         '20250127',
         array( 'strategy' => 'defer' )
-    );
+    );  
+
+    if ( is_page('contact')) {
+        wp_enqueue_script(
+            'mindset-scroll-to-top',
+            get_theme_file_uri('assets/js/scroll-to-top-contact.js'),
+            array(),
+            '20250127',
+            array( 'strategy' => 'defer' )
+        );
+            
+    }
 }
 add_action( 'wp_enqueue_scripts', 'mindset_enqueues' );
 
 function mindset_setup() {
 	add_editor_style( get_stylesheet_uri() );
 
-    // Crop images to 400px by 500px
+    // 4:5 aspect ratio
     add_image_size( '400x500', 400, 500, true );
-
-    // Crop images to 200px by 250px
     add_image_size( '200x250', 200, 250, true );
+
+    // 2:1 aspect ratio
+    add_image_size( '800x400', 800, 400, true );
+    add_image_size( '400x200', 400, 200, true );
+
 }
 add_action( 'after_setup_theme', 'mindset_setup' );
 
@@ -43,7 +57,12 @@ function mindset_add_custom_image_sizes( $size_names ) {
 	$new_sizes = array(
 		'400x500' => __( '400x500', 'mindset-theme' ),
 		'200x250' => __( '200x250', 'mindset-theme' ),
+        '400x200' => __( '400x200', 'mindset-theme' ),
+        '800x400' => __( '800x400', 'mindset-theme' ),
 	);
 	return array_merge( $size_names, $new_sizes );
 }
 add_filter( 'image_size_names_choose', 'mindset_add_custom_image_sizes' );
+
+// Load custom blocks.
+require get_theme_file_path() . '/mindset-blocks/mindset-blocks.php';
---
+++ mindset-blocks/build/company-address/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-i18n'), 'version' => '53b275d0c605eb6eadb8');
