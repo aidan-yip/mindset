230fc0e6ad1db96ad69e2be4519f7a29f07afad7
Aidan Y <jamesayipt6@gmail.com>
2025-02-02 22:24:59 -0800

    Day 6 afternoon tasks

--- mindset-blocks/mindset-blocks.php
+++ mindset-blocks/mindset-blocks.php
@@ -10,6 +10,9 @@ function mindset_blocks_mindset_blocks_block_init() {
 	register_block_type( __DIR__ . '/build/copyright' );
 	register_block_type( __DIR__ . '/build/company-address' );
 	register_block_type( __DIR__ . '/build/company-email' );
+	register_block_type( __DIR__ . '/build/service-posts', array(
+		'render_callback' => 'fwd_render_service_posts',
+	));
 }
 add_action( 'init', 'mindset_blocks_mindset_blocks_block_init' );
 
@@ -39,3 +42,52 @@ function mindset_register_custom_fields() {
 	);
 }
 add_action( 'init', 'mindset_register_custom_fields' );
+
+function fwd_render_service_posts( $attributes ) {
+    ob_start();
+    ?>
+    <div <?php echo get_block_wrapper_attributes(); ?>>
+        <?php
+        
+        $args = array( 
+			'post_type'      => 'service',
+			'posts_per_page' => 4
+		);
+
+		$query = new WP_Query( $args );
+		 
+		if ( $query -> have_posts() ) {
+			while( $query -> have_posts() ) {
+				$query -> the_post();
+				
+				echo '<article>';
+					echo '<a href="' . get_permalink() . '">' . get_the_title() . '</a>'; // Title in a with permalink
+					the_post_thumbnail(); // Featured Image
+					the_content(); // Content from block editor
+				echo '</article>';
+		 
+			}
+			wp_reset_postdata(); 
+		}
+		 
+		$query = new WP_Query( $args );
+		 
+		if ( $query -> have_posts() ) {
+			while( $query -> have_posts() ) {
+				$query -> the_post();
+				
+				echo '<article>';
+					echo '<h2>' . get_the_title() . '</h2>'; // Title in h2
+					the_post_thumbnail(); // Featured Image
+					the_content(); // Content from block editor
+				echo '</article>';
+		 
+			}
+			wp_reset_postdata(); 
+		}
+        
+        ?>
+    </div>
+    <?php
+    return ob_get_clean();
+}
